ed464546a74a3f3dfc51a99b16a561233fc6d330
abhishekgaud7 <139644889+abhishekgaud7@users.noreply.github.com>
2023-10-06 15:24:05 +0530

    Add files via upload

---
+++ array addition.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+	int arr1[5]={7,6,3,6,6};
+	int arr2[5]={6,3,9,8,7};
+	printf("%d",arr1 + arr2);
+}
---
+++ array length.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+main()
+{
+	int []arr={3,6,4,7,4,67,88,86,7,6,6,6};
+	printf("%d",arr.length-1);
+}
